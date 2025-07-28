729df81800de1b7b8a1482a400ef9920c348a8a3
ProgWrite <dimkakuz123@gmail.com>
2025-07-28 20:27:58 +0300

    feat(auth): add User model and migration
    - Created User entity with id, login, password fields
    - Added Flyway migration script (V1)

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,31 @@
+package org.example.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
