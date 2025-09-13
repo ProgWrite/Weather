4338a181b25e1a2d59615990784131a9f3a9e8ba
ProgWrite <dimkakuz123@gmail.com>
2025-09-13 09:16:31 +0300

    refactor: - translate russian words to english in index.html. - add max size in field name in LocationRequestDto

--- src/main/java/org/example/dto/LocationRequestDto.java
+++ src/main/java/org/example/dto/LocationRequestDto.java
@@ -3,6 +3,7 @@ package org.example.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor
@@ -12,6 +13,7 @@ import lombok.*;
 @ToString
 public class LocationRequestDto {
     @NotBlank(message = "The location name can't be blank")
+    @Size(max = 60, message = "The location name must not exceed 60 characters.")
     @Pattern(regexp = "^[a-zA-Zа-яА-ЯёЁ\\-]+$", message = "The location name must contain only letters (English or Russian) or hyphen")
     String name;
 }
